f4b77877c6f7beac9e8c97706981985d4fe14ec6
kong <kong@gmail.com>
2023-02-07 14:33:10 +0700

    1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
 void stat(const double[],int,double[]);
@@ -17,3 +18,33 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+    double Ans = 0,Ans2 = 0, Ans3 =1 ;
+    for(int n=0;n<N; n++){
+        Ans+=A[n];
+        
+    }B[0]=Ans/N;
+
+    for(int n=0;n<N; n++){
+        Ans2+=pow(A[n]-B[0],2);
+        B[1]=sqrt(Ans2/N);
+
+    }
+
+   for(int n=0;n<N; n++){
+        Ans3*=A[n];
+        
+    }B[2]=pow(Ans3,1.0/N);
+    
+    Ans =0;
+    for(int n=0;n<N; n++){
+        Ans+=1.0/A[n];
+        }B[3]=N/Ans;
+        
+    B[4]=*max_element(A,A+N);
+    B[5]=*min_element(A,A+N);
+    
+
+
+}
+
